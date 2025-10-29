ade0dc91b29c27c9cb2cfb7da498249429c6d4d8
D segura <dsegura629@hmail.com>
2025-10-29 16:40:53 -0500

    crud autores y libros

---
+++ routers/router_autores.py
@@ -0,0 +1,37 @@
+from fastapi import APIRouter, status
+from database import engine
+import crud
+from models import Autor, AutorCreate, AutorUpdate
+from sqlmodel import Session
+
+router = APIRouter(prefix="/autores", tags=["Autores"])
+
+@router.post("/", response_model=Autor, status_code=status.HTTP_201_CREATED)
+def crear(new_autor: AutorCreate):
+    with Session(engine) as session:
+        return crud.crear_autor(session, new_autor)
+
+@router.get("/", response_model=list[Autor])
+def listar(nacionalidad: str = None):
+    with Session(engine) as session:
+        return crud.listar_autores(session, nacionalidad)
+
+@router.get("/{id_autor}", response_model=Autor)
+def obtener(id_autor: int):
+    with Session(engine) as session:
+        return crud.obtener_autor(session, id_autor)
+
+@router.patch("/{id_autor}", response_model=Autor)
+def actualizar(id_autor: int, datos: AutorUpdate):
+    with Session(engine) as session:
+        return crud.actualizar_autor(session, id_autor, datos)
+
+@router.delete("/{id_autor}")
+def eliminar(id_autor: int):
+    with Session(engine) as session:
+        return crud.eliminar_autor(session, id_autor)
+
+@router.get("/{id_autor}/libros")
+def libros_de_autor(id_autor: int):
+    with Session(engine) as session:
+        return crud.libros_de_autor(session, id_autor)
---
+++ routers/router_libros.py
@@ -0,0 +1,37 @@
+from fastapi import APIRouter, status
+from database import engine
+import crud
+from models import Libro, LibroCreate, LibroUpdate, Autor
+from sqlmodel import Session
+
+router = APIRouter(prefix="/libros", tags=["Libros"])
+
+@router.post("/", response_model=Libro, status_code=status.HTTP_201_CREATED)
+def crear(new_libro: LibroCreate):
+    with Session(engine) as session:
+        return crud.crear_libro(session, new_libro)
+
+@router.get("/", response_model=list[Libro])
+def listar(genero: str = None, codigo: int = None):
+    with Session(engine) as session:
+        return crud.listar_libros(session, genero, codigo)
+
+@router.get("/{libro_id}", response_model=Libro)
+def obtener(libro_id: int):
+    with Session(engine) as session:
+        return crud.obtener_libro(session, libro_id)
+
+@router.patch("/{libro_id}", response_model=Libro)
+def actualizar(libro_id: int, datos: LibroUpdate):
+    with Session(engine) as session:
+        return crud.actualizar_libro(session, libro_id, datos)
+
+@router.delete("/{libro_id}")
+def eliminar(libro_id: int):
+    with Session(engine) as session:
+        return crud.eliminar_libro(session, libro_id)
+
+@router.get("/{libro_id}/autor", response_model=Autor)
+def autor_del_libro(libro_id: int):
+    with Session(engine) as session:
+        return crud.autor_del_libro(session, libro_id)
